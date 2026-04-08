0a8e771a3519629b96e32f3177e5dee21d7211b2
nashthecoder <naijeria@gmail.com>
2026-04-08 10:00:11 +0300

    Add CTA to countdown page, improve logo sizing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,17 @@
 class HomeController < ApplicationController
   allow_unauthenticated_access
-  layout "launch", only: [ :loader, :app, :countdown ]
+  layout "launch", only: [:loader, :app, :countdown]
 
   def app
-    return if !authenticated? || current_user.nil?
-    redirect_to onboarding_path(1) unless current_user.onboarding_completed?
+    redirect_to onboarding_path(1) if authenticated? && !current_user&.onboarding_completed?
   end
 
   def loader
   end
 
   def welcome
-    return if !authenticated? || current_user.nil?
-    redirect_to user_root_path if current_user.onboarding_completed?
-    redirect_to onboarding_path(1) unless current_user.onboarding_completed?
+    redirect_to user_root_path if authenticated? && current_user&.onboarding_completed?
+    redirect_to onboarding_path(1) if authenticated? && !current_user&.onboarding_completed?
   end
 
   def countdown
