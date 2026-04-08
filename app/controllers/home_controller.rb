67c21cb65032c2c10e509e43d0870a7195ef6e21
nashthecoder <naijeria@gmail.com>
2026-04-08 10:04:05 +0300

    Remove auto-login redirect from welcome page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   allow_unauthenticated_access
-  layout "launch", only: [:loader, :app, :countdown]
+  layout "launch", only: [:loader, :app, :countdown, :welcome]
 
   def app
     redirect_to onboarding_path(1) if authenticated? && !current_user&.onboarding_completed?
@@ -10,8 +10,6 @@ class HomeController < ApplicationController
   end
 
   def welcome
-    redirect_to user_root_path if authenticated? && current_user&.onboarding_completed?
-    redirect_to onboarding_path(1) if authenticated? && !current_user&.onboarding_completed?
   end
 
   def countdown
