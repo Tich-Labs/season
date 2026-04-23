7f738f5c70e72cb2e370ee2eb92ea2d02c510759
nashthecoder <naijeria@gmail.com>
2026-04-23 15:56:25 +0300

    Update password reset email sender and log outgoing mail

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,12 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "Season App <info@season.vision>"
+  default from: "Season App <seasonfemcycleapp@gmail.com>"
   layout "mailer"
+
+  private
+
+  def mail(headers = {}, &block)
+    message = super
+    Rails.logger.info "ActionMailer outgoing mail from=#{Array(message.from).join(', ')} to=#{Array(message.to).join(', ')} subject=#{message.subject}"
+    message
+  end
 end
