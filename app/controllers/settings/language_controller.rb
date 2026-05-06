38dd1d36a997d18012f8441b425830c5589a06ed
TichLabs <tichlabs@192.168.1.12>
2026-05-06 17:52:06 +0300

    fix: Use correct path helper settings_language_path

--- app/controllers/settings/language_controller.rb
+++ app/controllers/settings/language_controller.rb
@@ -9,7 +9,7 @@ class Settings::LanguageController < ApplicationController
     @user = current_user
     if @user.update(language: params[:user][:language])
       I18n.locale = @user.language.to_sym
-      redirect_to language_settings_path, notice: t("settings.language.updated")
+      redirect_to settings_language_path, notice: t("settings.language.updated")
     else
       render :show, status: :unprocessable_content
     end
