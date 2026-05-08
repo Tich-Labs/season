7de31cceed29720b5fb8c968c0ebdbf77ad08f22
TichLabs <tichlabs@192.168.1.12>
2026-05-09 02:15:40 +0300

    feat: Complete iOS project with AppDelegate, Info.plist, LaunchScreen, Assets

--- ios/SeasonApp/SeasonApp/AppDelegate.swift
+++ ios/SeasonApp/SeasonApp/AppDelegate.swift
@@ -8,19 +8,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
-        let window = UIWindow(frame: UIScreen.main.bounds)
-        self.window = window
+        window = UIWindow(frame: UIScreen.main.bounds)
+        guard let window = window else { return false }
 
-        // Create WebView
         let webView = WKWebView(frame: window.bounds)
         webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
 
-        // Load Season app
         let url = URL(string: "https://seasonv2.onrender.com")!
-        let request = URLRequest(url: url)
-        webView.load(request)
+        webView.load(URLRequest(url: url))
 
-        // Create view controller
         let viewController = UIViewController()
         viewController.view.addSubview(webView)
 
