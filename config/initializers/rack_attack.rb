d9cd560cf4993109f569c52fdf77764a27eae2c0
TichLabs <tichlabs@192.168.1.3>
2026-05-22 13:02:42 +0300

    Fix rack-attack throttle DSL (requires Rack::Attack.throttle in v6.x)

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -4,28 +4,25 @@
 if defined?(Rack::Attack)
   Rack::Attack.enabled = !Rails.env.test?
 
-  # Throttle login attempts by IP
-  throttle("logins/ip", limit: 10, period: 60) do |req|
+  Rack::Attack.throttle("logins/ip", limit: 10, period: 60) do |req|
     req.ip if req.post? && req.path == "/session"
   end
 
-  # Throttle registration attempts by IP
-  throttle("registrations/ip", limit: 5, period: 60) do |req|
+  Rack::Attack.throttle("registrations/ip", limit: 5, period: 60) do |req|
     req.ip if req.post? && req.path == "/registration"
   end
 
-  # Throttle launch signups by IP
-  throttle("launch_signups/ip", limit: 10, period: 300) do |req|
+  Rack::Attack.throttle("launch_signups/ip", limit: 10, period: 300) do |req|
     req.ip if req.post? && req.path == "/launch-signup"
   end
 
-  # Throttle password reset requests by IP
-  throttle("password_resets/ip", limit: 3, period: 300) do |req|
+  Rack::Attack.throttle("password_resets/ip", limit: 3, period: 300) do |req|
     req.ip if req.post? && req.path == "/users/password"
   end
 
-  # Block suspicious IPs after 40 requests in 1 minute
-  throttle("block/ip", limit: 40, period: 60) do |req|
-    req.ip
+  Rack::Attack.blocklist("block/ip") do |req|
+    Rack::Attack::Allow2Ban.filter("block-#{req.ip}", maxretry: 40, findtime: 1.minute, bantime: 1.hour) do
+      true
+    end
   end
 end
