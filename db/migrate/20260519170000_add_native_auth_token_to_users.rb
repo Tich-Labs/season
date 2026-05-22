4d06f00f423b4b8ca17a1db4a0f81f7e84ddafc8
TichLabs <tichlabs@192.168.1.3>
2026-05-22 13:34:53 +0300

    Remove duplicate native_auth_token migration

--- db/migrate/20260519170000_add_native_auth_token_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddNativeAuthTokenToUsers < ActiveRecord::Migration[8.1]
-  def change
-    add_column :users, :native_auth_token, :string
-    add_index :users, :native_auth_token, unique: true
-  end
-end
