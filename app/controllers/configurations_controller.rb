43b7c73b5e54429e7a9dddc3448374e107bd390a
TichLabs <tichlabs@192.168.1.14>
2026-05-26 23:07:41 +0300

    Remove invalid tab_bar_hidden property from path config

--- app/controllers/configurations_controller.rb
+++ app/controllers/configurations_controller.rb
@@ -9,13 +9,6 @@ class ConfigurationsController < ApplicationController
       },
       rules: [
         {
-          patterns: ["/", "/welcome", "/session/*", "/registration/*", "/onboarding/*", "/launch-signup", "/password/*", "/invite/*"],
-          properties: {
-            "presentation" => "default",
-            "tab_bar_hidden" => true
-          }
-        },
-        {
           patterns: ["/calendar", "/tracking", "/daily/*", "/symptoms", "/superpowers"],
           properties: {
             "presentation" => "default"
@@ -38,13 +31,6 @@ class ConfigurationsController < ApplicationController
         "tab_bar_tint_color" => "#933a35"
       },
       rules: [
-        {
-          patterns: ["/", "/welcome", "/session/*", "/registration/*", "/onboarding/*", "/launch-signup", "/password/*", "/invite/*"],
-          properties: {
-            "presentation" => "default",
-            "tab_bar_hidden" => true
-          }
-        },
         {
           patterns: ["/calendar", "/tracking", "/daily/*", "/symptoms", "/superpowers"],
           properties: {
