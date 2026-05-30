0367aa08e29183981099b5a89c6eb10c25208aed
TichLabs <tichlabs@192.168.1.5>
2026-05-30 23:42:17 +0300

    Fix i18n locale not applying after language change: add set_locale before_action to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   include Authentication
   include TurboNativeDetection
+
+  before_action :set_locale, if: :current_user
+
+  private
+
+  def set_locale
+    I18n.locale = current_user.language.to_sym if current_user.language.present?
+  end
 end
